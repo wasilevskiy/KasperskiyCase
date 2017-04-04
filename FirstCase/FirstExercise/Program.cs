06a81f0e007c3443a9f0c2891a63f3f5661723fc
wasilevskiy <vasilevskii_ilya@mail.ru>
2017-04-05 02:59:51 +0300

    Project
    gg

---
+++ FirstCase/FirstExercise/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace FirstExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SafeQueue<int> TestQueue = new SafeQueue<int>();
+            Thread PushElement = new Thread(() => PopTest(20,TestQueue));
+            Thread PopElement = new Thread(() => PushTest(20,TestQueue));
+            PopElement.Start();
+            PushElement.Start();
+            Console.ReadLine();
+        }
+        static void PushTest(int n,SafeQueue<int> Queue)
+        {
+            Random r = new Random();
+            for (int i = 0; i < n; i++) 
+            {
+                Queue.Push(r.Next(1, 100));
+            }
+        }
+
+        static void PopTest(int n,SafeQueue<int> Queue)
+        {
+            for (int i = 0; i < n; i++) 
+            {
+                Queue.Pop();
+            }
+        }
+    }
+
+    class SafeQueue<T>
+    {
+        private readonly Queue<T> queue;
+        private readonly object thisLock;
+
+        public SafeQueue()
+        {
+            queue = new Queue<T>();
+            thisLock = new object();
+        }
+        public void Push(T element)
+        {
+            lock (thisLock)
+            {
+                queue.Enqueue(element);
+                Console.WriteLine("push: {0}", element);
+                Monitor.Pulse(thisLock);
+            }
+        }
+
+        public T Pop()
+        {
+            lock(thisLock)
+            {
+                while (queue.Count == 0)
+                {
+                    Monitor.Wait(thisLock);
+                }
+                T element = queue.Dequeue();
+                Console.WriteLine("Pop:{0}", element);
+                return element;
+            }
+        }
+    }
+}
---
+++ SecondCase/TwoNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите искомое число:");
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите минимальную границу: ");
+            var minValue = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальную границу:");
+            var maxValue = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество чисел:");
+            var Count = int.Parse(Console.ReadLine());
+            Hashtable table = new Hashtable();
+            //Считаем, что массив не заполнен => заполняем его
+            int[] array = InsertArr(minValue, maxValue, Count);
+
+            for (int i = 0;i < array.Length;i++)
+            {
+                int element = array[i];
+                int summ = number - element;
+                if (table.ContainsKey(element))
+                    Console.WriteLine(element + "," + table[element]);
+                else if(!table.ContainsKey(summ))
+                    table.Add(summ, element);
+            }
+            Console.ReadLine();
+        }
+
+        public static int[] InsertArr(int minValue,int maxValue, int Count)
+        {
+            int[] array = new int[Count];
+            Random r = new Random();
+            for(int i=0;i<array.Length;i++)
+            {
+                array[i] = r.Next(minValue, maxValue);
+            }
+            return array;
+        }
+    }
+}
