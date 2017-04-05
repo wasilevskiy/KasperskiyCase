2d40e56662141ad72e050424d9f516d63a79209b
wasilevskiy <vasilevskii_ilya@mail.ru>
2017-04-05 11:39:32 +0300

    Update Program.cs

--- SecondCase/TwoNumbers/Program.cs
+++ SecondCase/TwoNumbers/Program.cs
@@ -11,22 +11,18 @@ namespace TwoNumbers
             int minValue = 0;
             int maxValue = 0;
             int Count = 0;
-            PairsOfNumbers Pair = new PairsOfNumbers();
             //Считываем данные с клавиатуры
-            bool isNormal = Pair.GetInfo(number, minValue, maxValue, Count);
+            bool isNormal = GetInfo(number, minValue, maxValue, Count);
             if (isNormal)
             {
                 Hashtable table = new Hashtable();
                 //Считаем, что массив не заполнен => заполняем его
-                int[] array = Pair.InsertArr(minValue, maxValue, Count);
-                Pair.FindPairs(array, table, number);
+                int[] array = InsertArr(minValue, maxValue, Count);
+                FindPairs(array, table, number);
             }
             Console.ReadLine();
         }
-    }
-    public class PairsOfNumbers
-    {
-        public Hashtable FindPairs(int[] array, Hashtable table, int number)
+        static  Hashtable FindPairs(int[] array, Hashtable table, int number)
         {
             for (int i = 0; i < array.Length; i++)
             {
@@ -39,7 +35,7 @@ namespace TwoNumbers
             }
             return table;
         }
-        public int[] InsertArr(int minValue, int maxValue, int Count)
+        static int[] InsertArr(int minValue, int maxValue, int Count)
         {
             int[] array = new int[Count];
             Random r = new Random();
@@ -50,7 +46,7 @@ namespace TwoNumbers
             return array;
         }
 
-        public bool GetInfo(int num, int min, int max, int count)
+        static bool CheckInput(int num, int min, int max, int count)
         {
             try
             {
@@ -67,7 +63,7 @@ namespace TwoNumbers
                 }
                 Console.WriteLine("Введите количество чисел:");
                 count = int.Parse(Console.ReadLine());
-                
+
             }
             catch (Exception ex)
             {
@@ -76,6 +72,6 @@ namespace TwoNumbers
             }
             return true;
         }
+
     }
 }
-
