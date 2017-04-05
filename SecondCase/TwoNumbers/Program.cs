f164755f643bc7fd4c61889b74bc115a743139d0
wasilevskiy <vasilevskii_ilya@mail.ru>
2017-04-05 11:31:14 +0300

    Update Program.cs

--- SecondCase/TwoNumbers/Program.cs
+++ SecondCase/TwoNumbers/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 
 namespace TwoNumbers
@@ -7,39 +7,74 @@ namespace TwoNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите искомое число:");
-            var number = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите минимальную границу: ");
-            var minValue = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите максимальную границу:");
-            var maxValue = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите количество чисел:");
-            var Count = int.Parse(Console.ReadLine());
-            Hashtable table = new Hashtable();
-            //Считаем, что массив не заполнен => заполняем его
-            int[] array = InsertArr(minValue, maxValue, Count);
-
-            for (int i = 0;i < array.Length;i++)
+            int number = 0;
+            int minValue = 0;
+            int maxValue = 0;
+            int Count = 0;
+            PairsOfNumbers Pair = new PairsOfNumbers();
+            //Считываем данные с клавиатуры
+            bool isNormal = Pair.GetInfo(number, minValue, maxValue, Count);
+            if (isNormal)
+            {
+                Hashtable table = new Hashtable();
+                //Считаем, что массив не заполнен => заполняем его
+                int[] array = Pair.InsertArr(minValue, maxValue, Count);
+                Pair.FindPairs(array, table, number);
+            }
+            Console.ReadLine();
+        }
+    }
+    public class PairsOfNumbers
+    {
+        public Hashtable FindPairs(int[] array, Hashtable table, int number)
+        {
+            for (int i = 0; i < array.Length; i++)
             {
                 int element = array[i];
                 int summ = number - element;
                 if (table.ContainsKey(element))
-                    Console.WriteLine(element + "," + table[element]);
-                else if(!table.ContainsKey(summ))
+                    Console.WriteLine("Число: {0} дают в сумме два элемента: {1} и {2}", number, element, table[element]);
+                else if (!table.ContainsKey(summ))
                     table.Add(summ, element);
             }
-            Console.ReadLine();
+            return table;
         }
-
-        public static int[] InsertArr(int minValue,int maxValue, int Count)
+        public int[] InsertArr(int minValue, int maxValue, int Count)
         {
             int[] array = new int[Count];
             Random r = new Random();
-            for(int i=0;i<array.Length;i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 array[i] = r.Next(minValue, maxValue);
             }
             return array;
         }
+
+        public bool GetInfo(int num, int min, int max, int count)
+        {
+            try
+            {
+                Console.WriteLine("Введите искомое число:");
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите минимальную границу: ");
+                min = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите максимальную границу:");
+                max = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите количество чисел:");
+                count = int.Parse(Console.ReadLine());
+                if (max < min)
+                {
+                    Console.WriteLine("Ошибка! Максимальное значение не может быть меньше минимального!");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при вводе данных: " + ex.ToString());
+                return false;
+            }
+            return true;
+        }
     }
 }
+
